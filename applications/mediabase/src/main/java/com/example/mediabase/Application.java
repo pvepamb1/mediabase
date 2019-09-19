24000194626008c1391132e27b9389e2d282eac7
Prasenna Vepambedu <prasenna.vepambedu@perficient.com>
2019-09-19 10:13:50 -0400

    service discovery

--- applications/mediabase/src/main/java/com/example/mediabase/Application.java
+++ applications/mediabase/src/main/java/com/example/mediabase/Application.java
@@ -1,23 +1,20 @@
 package com.example.mediabase;
 
 import com.example.mediabase.podcastsui.PodcastClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 import com.example.mediabase.moviesui.ActionServlet;
 import com.example.mediabase.moviesui.MovieClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
-
-    @Value("${movies.ms.url}")
-    private String moviesURL;
-    @Value("${podcasts.ms.url}")
-    private String podcastURL;
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
@@ -29,17 +26,18 @@ public class Application {
     }
 
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
 
     @Bean
     public MovieClient movieClient(RestOperations restOperations) {
-        return new MovieClient(moviesURL, restOperations);
+        return new MovieClient("//movies-ms/movies", restOperations);
     }
 
     @Bean
     public PodcastClient podcastClient(RestOperations restOperations) {
-        return new PodcastClient(podcastURL, restOperations);
+        return new PodcastClient("//podcasts-ms/podcasts", restOperations);
     }
 }
